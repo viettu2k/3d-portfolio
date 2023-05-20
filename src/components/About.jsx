9cbfdb291dde5141b807800f4cf74301ecd69a6e
viettu2k <doanviettu2k@gmail.com>
2023-05-20 22:39:28 +0700

    remove unessary empty line

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ import { fadeIn, textVariant } from '../utils/motion';
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
     <motion.div
-      variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+      variants={fadeIn('right', 'spring', index * 0.5, 1)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
     >
       <div
@@ -25,7 +25,6 @@ const ServiceCard = ({ index, title, icon }) => (
           alt='web-development'
           className='w-16 h-16 object-contain'
         />
-
         <h3 className='text-white text-[20px] font-bold text-center'>
           {title}
         </h3>
